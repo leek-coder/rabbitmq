230e8d9f7cfc62b6835012513d6352cc1f868be1
like <13521819338@163.com>
2020-12-04 16:45:20 +0800

    [ADD]新建StudentController类

---
+++ src/main/java/com/huatech/rabbit/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.huatech.rabbit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 新增StudentController
+ *
+ * @author like
+ * @date 2020-12-04 4:43 下午
+ **/
+@Controller
+@RequestMapping(value = "/student")
+public class StudentController {
+}
