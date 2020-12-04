2387cfc2e944ec440a2bc44892567a447ebb2914
like <13521819338@163.com>
2020-12-04 16:50:58 +0800

    [ADD]新增商品日志打印

--- src/main/java/com/huatech/rabbit/controller/ProductController.java
+++ src/main/java/com/huatech/rabbit/controller/ProductController.java
@@ -1,8 +1,18 @@
 package com.huatech.rabbit.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author like
  * @date 2020-12-04 4:45 下午
  **/
+
+@Controller
+@RequestMapping(value = "/product")
 public class ProductController {
+    private Logger logger = LoggerFactory.getLogger(ProductController.class);
+
 }
