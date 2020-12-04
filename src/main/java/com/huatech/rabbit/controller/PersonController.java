33cdc173919e72c03aba3a8633b3591341283762
like <13521819338@163.com>
2020-12-04 16:40:55 +0800

    [ADD]新建PersonController类

---
+++ src/main/java/com/huatech/rabbit/controller/PersonController.java
@@ -0,0 +1,16 @@
+package com.huatech.rabbit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 用户控制器
+ *
+ * @author like
+ * @date 2020-12-04 4:40 下午
+ **/
+@Controller
+@RequestMapping(value = "/person")
+public class PersonController {
+
+}
